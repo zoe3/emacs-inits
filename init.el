015a5aba0505742f5ac802da9a3ae8560b12fe0a
zoe3 <vaguelyr.preciselyw.317@gmail.com>
2020-04-27 04:54:19 +0000

    move and delete

--- init.el
+++ init.el
@@ -1,18 +1,67 @@
-;; ELPA
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  (package-initialize)
-  (unless (package-installed-p 'init-loader)
-    (package-refresh-contents)
-    (package-install 'init-loader))
-  (require 'init-loader)
-  (init-loader-load))
+;;; MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
 
-(when (equal system-type 'windows-nt)
-  (load-file "~/.emacs.d/init-windows-nt.el"))
+;;; 環境
+(setq inhibit-startup-message t)
+(set-locale-environment nil)
+(set-language-environment 'Japanese)
+(prefer-coding-system 'utf-8)
 
-(when (equal system-type 'gnu/linux)
-  (load-file "~/.emacs.d/init-linux.el"))
+;; Emacs停止確認
+(setq confirm-kill-emacs 'y-or-n-p)
 
+;; temporary-file-directory
+(setq temporary-file-directory (directory-file-name "~/.emacs.d/temp"))
+
+;; scratch
+(setq initial-scratch-message "")
+
+;;; 表示
+;; ウィンドウを左右に分割したとき用の設定
+(setq-default truncate-partial-width-windows t)
+;; 改行の文字
+(setq-default fill-column 95)
+(setq auto-fill-mode nil)
+
+;; カーソルの位置を表示する
+(column-number-mode t)
+(line-number-mode t)
+;; ファイルサイズを表示
+(size-indication-mode t)
+;; 時刻表示
+(display-time-mode t)
+
+;;; ハイライト
+;; 括弧の中身をハイライト
+(show-paren-mode t)
+;; カーソルの点滅を止める
+(blink-cursor-mode 0)
+
+;;  evalした結果を全部表示
+(setq eval-expression-print-length nil)
+;; 現在の行を目立たせる
+(global-hl-line-mode)
+
+;;; バックアップとオートセーブ
+;; バックアップファイルを作らない
+(setq backup-inhibited t)
+;; 終了時にオートセーブファイルを消す
+(setq delte-auto-save-files t)
+
+;;; dired
+;; ffap
+(ffap-bindings)
+;; 2
+(setq dired-dwim-target t)
+
+;; dired-mode editable
+(add-hook 'dired-load-hook (lambda ()
+                  (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)))
+
+;;; キーバインドの設定
+;; 折り返しトグルコマンド
+(define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
 
