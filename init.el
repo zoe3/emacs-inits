9a38eb7d3efba540b5e8f2e22490b1c23abdf05d
zoe3 <vaguelyr.preciselyw.317@gmail.com>
2020-07-13 13:02:48 +0900

    add ddskk

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;; MELPA
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 ;;; 環境
@@ -73,6 +73,12 @@
   (setq python-shell-interpreter "ipython3"
       python-shell-interpreter-args "-i --simple-prompt"))
 
+;;; DDSKK
+(setq skk-user-directory "~/.emacs.d/ddskk/")
+
+(global-set-key "\C-x\C-j" 'skk-mode)
+(global-set-key "\C-xj" 'skk-auto-fill-mode)
+(global-set-key "\C-xt" 'skk-tutorial)
 
 
 (custom-set-variables
@@ -80,7 +86,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (jedi elpy use-package init-loader))))
+ '(package-selected-packages
+   (quote
+    (ddskk ddskk-posframe jedi elpy use-package init-loader))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
